0985708514a518542a6b1780598f686afbb0a646
luandt <theluan143@gmail.com>
2021-06-21 19:59:30 +0700

    rename LP name

--- contracts/VSwapERC20.sol
+++ contracts/VSwapERC20.sol
@@ -7,7 +7,7 @@ contract VSwapERC20 is IVSwapERC20 {
     using SafeMath for uint;
 
     string public constant name = 'VSwap LPs';
-    string public constant symbol = 'Cake-LP';
+    string public constant symbol = 'VSwap-LP';
     uint8 public constant decimals = 18;
     uint  public totalSupply;
     mapping(address => uint) public balanceOf;
